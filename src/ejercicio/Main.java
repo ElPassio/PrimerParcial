abe4005892d1cd842136a98e88fb07e49765286b
Passio <passiotti.agustin@gmail.com>
2024-05-23 16:10:32 -0300

    cambios p

--- src/ejercicio/Main.java
+++ src/ejercicio/Main.java
@@ -2,7 +2,7 @@ package ejercicio;
 import java.util.Scanner;
 
 public class Main {
-
+	public static final int maxVec = 10;
 	public static int menu(Scanner scan) {
 		System.out.println("Bienvenido al sistema de Emergencias\n\n"
 							+ "1) A\n"
--- src/ejercicio/Tienda.java
+++ src/ejercicio/Tienda.java
@@ -1,8 +1,16 @@
 package ejercicio;
 
-public class Tienda {
+public class Tienda implements Facturacion extends Empresa{
 	private int sucursal;
 	public Articulo inventario[];
 	public Pedido pedidosClientes[];
 	public Transaccion transacciones[];
+	public Tienda(int sucursal, Articulo[] inventario, Pedido[] pedidosClientes, Transaccion[] transacciones, String nombre, String cuit) {
+		super(nombre, cuit);
+		this.sucursal = sucursal;
+		this.inventario = inventario;
+		this.pedidosClientes = pedidosClientes;
+		this.transacciones = transacciones;
+	}
+	
 }
