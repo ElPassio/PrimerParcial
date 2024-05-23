ea9623c6b5c05219ea2c683aa234a15bfc114dc7
darihub <gramarg85@gmail.com>
2024-05-23 16:11:32 -0300

    pedido y transaccion

---
+++ src/ejercicio/Transaccion.java
@@ -0,0 +1,54 @@
+package ejercicio;
+
+public class Transaccion {
+	private int id;
+	private Pedido pedido;
+	private String estado;
+	private String fechaPago;
+	private double montoTotal;
+	
+	public Transaccion() {
+		id = 0; pedido = new Pedido(); estado = ""; fechaPago = ""; montoTotal=0;
+	}
+	public Transaccion(int id, Pedido pedido, String estado, String fechaPago, double montoTotal) {
+		super();
+		this.id = id;
+		this.pedido = pedido;
+		this.estado = estado;
+		this.fechaPago = fechaPago;
+		this.montoTotal = montoTotal;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getFechaPago() {
+		return fechaPago;
+	}
+	public void setFechaPago(String fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+	public double getMontoTotal() {
+		return montoTotal;
+	}
+	public void setMontoTotal(double montoTotal) {
+		this.montoTotal = montoTotal;
+	}
+	
+	
+}
