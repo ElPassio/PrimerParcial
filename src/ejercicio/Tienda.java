d04924899bee2e87d93424d1ad3b5ba1cd6953bf
darihub <gramarg85@gmail.com>
2024-05-23 16:22:56 -0300

    tienda terminado

--- src/ejercicio/Tienda.java
+++ src/ejercicio/Tienda.java
@@ -1,10 +1,19 @@
 package ejercicio;
 
-public class Tienda implements Facturacion extends Empresa{
+public class Tienda extends Empresa implements Facturacion{
 	private int sucursal;
 	public Articulo inventario[];
 	public Pedido pedidosClientes[];
 	public Transaccion transacciones[];
+	
+	public Tienda() {
+		super();
+		sucursal = 0; inventario = new Articulo[Main.maxVec];
+		pedidosClientes = new Pedido[Main.maxVec];
+		transacciones = new Transaccion[Main.maxVec]; // quizas reemplazar maxVec
+		
+	}
+	
 	public Tienda(int sucursal, Articulo[] inventario, Pedido[] pedidosClientes, Transaccion[] transacciones, String nombre, String cuit) {
 		super(nombre, cuit);
 		this.sucursal = sucursal;
@@ -13,4 +22,7 @@ public class Tienda implements Facturacion extends Empresa{
 		this.transacciones = transacciones;
 	}
 	
+	public void emitirFactura(Transaccion transaccion) {
+		
+	}
 }
