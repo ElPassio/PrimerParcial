33b133dc9af887156b0d221accf528f2f899a9e5
darihub <gramarg85@gmail.com>
2024-05-23 18:07:36 -0300

    error arreglado

--- src/ejercicio/Main.java
+++ src/ejercicio/Main.java
@@ -32,19 +32,19 @@ public class Main {
 		Pedido pedidosClientes[] = new Pedido[maxVec];
 		Transaccion transacciones[] = new Transaccion[maxVec];
 		Empresa tienda = new Tienda(1, inventario, pedidosClientes, transacciones, "PassioShop", "20-42324345-4");
-		// ARTICULOS
+		// ARTICULOS DE LA TIENDA
 		Articulo comput = new Computadora("RYZEN 3600, RTX 5500, 12 GB RAM", "AMD", 2324, "Computadora", 3500);
 		
 		// CLIENTE
 		Cliente cl1 = new Cliente("Dario", "Debesa", 45465012);
 		// PEDIDO DEL CLIENTE
-		Pedido artcl1 = new Articulo();
-		artcl1.pedido[0] = comput;
-		Pedido pedcl1 = new Pedido(12, comput)
+		Articulo artcl1[] = new Articulo[1];
+		artcl1[0] = comput;
+		Pedido pedcl1 = new Pedido(125, artcl1, artcl1[0].getPrecio(), "23/5/2024");
 		// TRANSACCION
-		Transaccion transac1 = new Transaccion(12, )
+		Transaccion transac1 = new Transaccion(12, pedcl1, "", "23/5/2024", pedcl1.getCotizacionTotal());
 		// VENTA
-		Venta venta1 = new Venta(cl1, tienda);
+		Venta venta1 = new Venta(tienda, cl1, 1, pedcl1, "", "23/5/2024", transac1.getMontoTotal());
 		
 		
 		/*int opc = 1;
