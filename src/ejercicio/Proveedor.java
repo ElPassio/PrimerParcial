4457049a54ac481af0ed6c1359916cfa7a513aee
Passio <passiotti.agustin@gmail.com>
2024-05-23 16:51:12 -0300

    comence cambios proveedor

--- src/ejercicio/Proveedor.java
+++ src/ejercicio/Proveedor.java
@@ -1,37 +1,52 @@
 package ejercicio;
 
-public class Proveedor extends Empresa{
+public class Proveedor extends Empresa implements Facturacion {
 	private String nombreContacto;
 	private String telefonoContacto;
 	private Pedido pedidostienda[];
+
 	public Proveedor() {
 		super();
 		this.nombreContacto = "";
 		this.telefonoContacto = "";
 		this.pedidostienda = new Pedido[Main.maxVec];
 	}
-	public Proveedor(String nombreContacto, String telefonoContacto, Pedido[] pedidostienda, String nombre, String cuit) {
+
+	public Proveedor(String nombreContacto, String telefonoContacto, Pedido[] pedidostienda, String nombre,
+			String cuit) {
 		super(nombre, cuit);
 		this.nombreContacto = nombreContacto;
 		this.telefonoContacto = telefonoContacto;
 		this.pedidostienda = pedidostienda;
 	}
+	public void emitirFactura(Transaccion transaccion) {
+		if(transaccion.getMontoTotal()){}
+	}
 	public String getNombreContacto() {
 		return nombreContacto;
 	}
+
 	public void setNombreContacto(String nombreContacto) {
 		this.nombreContacto = nombreContacto;
 	}
+
 	public String getTelefonoContacto() {
 		return telefonoContacto;
 	}
+
 	public void setTelefonoContacto(String telefonoContacto) {
 		this.telefonoContacto = telefonoContacto;
 	}
+
 	public Pedido[] getPedidostienda() {
 		return pedidostienda;
 	}
+
 	public void setPedidostienda(Pedido[] pedidostienda) {
 		this.pedidostienda = pedidostienda;
+	}
+
+	public void despacharPedidos() {
+
 	}
 }
